10bc76c19e4880d843dbe79e85402652e8adf358
KatiePelletier <pellek1@mcmaster.ca>
2019-01-16 17:03:35 -0500

    With Fly_ID

--- Week2_Homework.R
+++ Week2_Homework.R
@@ -20,10 +20,12 @@ wings2 <- separate(wings1, "File", into= c("Crap", "Perp", "Line", "Slide", "Sex
 wings3 <- wings2[, -c(1)]
 #names(wings3)
 
-#Making the wings object something easier to type
-wings <- wings3
-#names(wings)
-#summary(wings)
+
+#however the fly ID is split into to cols (Slide and FLy). I want to merge these into a single Col so I have a fly ID that gives the slide/plate each fly is assosiated with and the position on that slide and plate 
+wings4 <- wings3 %>% unite(Fly_ID, Slide, Fly)
+names(wings4)
+
+wings <- wings4
 
 #Making line and sex and scale factors 
 levels(wings$Line) <- c("ef81", "ef96", "ef43", "zi192", "zi251", "zi418")
@@ -32,6 +34,11 @@ levels(wings$Sex) <- c("f", "m")
 wings$Sex <- factor(wings$Sex)
 wings$Scale <- factor(wings$Scale)
 levels(wings_raw$Scale) <- c("0.01134", "0.00694")
+
+#I want to make sure that the fly IDs are unique within each line (can be repeated between lines becuase slide 1, fly 1 exists for every genotype)
+print(wings
+      %>% group_by(Line)
+      %>% unique(Fly_ID))
 
 #Using a boxplot I want to look for outliers in centroid size 
 ggplot(wings, aes(y = CS, x = Line, color = Sex)) + 
